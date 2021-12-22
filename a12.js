80c6734f8ad4532b6b4577d1a4355ca74ec3078e
oculi-s <kjunhun9@sogang.ac.kr>
2021-12-22 16:56:00 +0900

    211222

--- a12.js
+++ a12.js
@@ -92,8 +92,8 @@ function indexing(num, i) {
     var d = H[i].tagName[1];
     var id = '';
     H[i].id = s[d][0] + num + s[d][1];
-    H[i].innerHTML = `<a href="#index">${H[i].id}</a>` + H[i].innerHTML;
-    if (d != H[i + 1].tagName) {
+    H[i].innerHTML = `<a href="#index">${H[i].id}</a> ` + H[i].innerHTML;
+    if (d != H[i + 1].tagName[1]) {
         indexing(1, i + 1);
     } else {
         indexing(num + 1, i + 1);
@@ -108,7 +108,7 @@ function setIndex() {
         var temp = '';
         indexing(1, 0);
         for (var i = 0; i < H.length; i++) {
-            temp += `<${H[i].tagName}><a href="#${H[i].id}">${H[i].id}</a>${H[i].innerText.toLowerCase()}</${H[i].tagName}>`
+            temp += `<${H[i].tagName}><a href="#${H[i].id}">${H[i].id}</a> ${H[i].innerText.toLowerCase()}</${H[i].tagName}>`
         }
         $('index').innerHTML = temp;
     }
