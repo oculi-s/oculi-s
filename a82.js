76a9a4096cd8ba5bda38c2d30d043729f8ac6427
oculi-s <kjunhun9@sogang.ac.kr>
2022-01-06 20:45:16 +0900

    220106

--- a82.js
+++ a82.js
@@ -81,10 +81,9 @@ console.log(url);
     fb.dict = fb.dict.data();
     fb.user = await getDoc(doc(db, 'user', ss.uid));
     fb.user = fb.user.data();
-    alert(JSON.stringify(fb.user));
-    alert(ss.uid);
-    nav.innerHTML = de(fb.srce.nav[ss.uid in fb.user]);
-    aside.innerHTML = de(fb.srce.aside[ss.uid in fb.user]);
+    alert(fb.user);
+    nav.innerHTML = de(fb.srce.nav[fb.user != null]);
+    aside.innerHTML = de(fb.srce.aside[fb.user != null]);
     wresize();
 })().then(() => {
     document.addEventListener('keydown', e => {
