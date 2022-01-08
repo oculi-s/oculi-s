02df7ea52f2cff9c2659a8a1022918aa44d62cf3
oculi-s <kjunhun9@sogang.ac.kr>
2022-01-08 16:41:23 +0900

    220108

--- a95.js
+++ a95.js
@@ -294,6 +294,7 @@ function edit() {
             save();
             clearInterval(int);
         } else if (k == 18) {
+            $$('es>div>span')[1].innerHTML = edit.dataset.eng == 'true' ? '영' : '한';
             e.preventDefault();
             if (edit.dataset.eng == 'true') {
                 edit.removeEventListener('keydown', listener);
